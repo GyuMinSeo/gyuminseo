5a57e472752354cfbcd2f7ec4c2cfa3fb867eb0d
ParkGyumin <gyumin1129@kookmin.ac.kr>
2024-09-17 12:15:03 +0900

    페이지 추가 실험

--- app.py
+++ app.py
@@ -1,7 +1,21 @@
 import streamlit as st
 
-# 버튼 클릭 시 app2.py로 리디렉션하는 방법
-if st.button('Go to App2'):
-    st.write("""
-        <meta http-equiv="refresh" content="0; url=app2.py">
-    """, unsafe_allow_html=True)
+if 'page' not in st.session_state:
+    st.session_state.page = 'home'
+
+# 페이지 상태 변경 함수
+def go_to_page(page_name):
+    st.session_state.page = page_name
+    st.experimental_rerun()  # 상태 변경 후 페이지 새로고침
+
+if st.session_state.page == 'home':
+    st.title("Home Page")
+    
+    if st.button("Go to App2"):
+        go_to_page('app2')
+    
+elif st.session_state.page == 'app2':
+    st.title("App2 Page")
+    
+    if st.button("Go back to Home"):
+        go_to_page('home')
